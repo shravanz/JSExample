4d1bff7fba66792ba1b6d7c921e6274c7ad9cd52
Shravan Zolgikar <shravan.zolgikar@innoflexion.com>
2018-01-04 13:40:51 +0530

    added reverseString

---
+++ ReversString.js
@@ -0,0 +1,26 @@
+//Write a Function to Reverse String with help of a function.
+
+//Method 1 using split(),Reverse(),join()
+function ReverseMyString(str) {
+    return str.split('').reverse().join('');
+};
+
+
+//Method 2 using traditional Forloop
+function ReverseMyString(str) {
+    let reversed = '';
+    for (let char of str) {
+        reversed = char + reversed
+    };
+    return reversed;
+};
+
+//Method 3 using array reduce method
+function ReverseMyString(str) {
+    return str.split('').reduce(function (char, rev) {
+        return rev + char;
+    }, '');
+};
+
+console.log(ReverseMyString('shravan'));
+//Output ==>   navarhs
