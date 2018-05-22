fd610f30481e008eff3f12dd43f27d27870b86a1
shravanz <shravanzolgikar@yahoo.in>
2018-05-22 16:07:42 +0530

    added example

--- 10bubblesort.js
+++ 10bubblesort.js
@@ -18,3 +18,14 @@ function bubblesort(array) {
 console.log(bubblesort([5, 4, 1, 8, 8, 6]));
 
 //Output -->[ 1, 4, 5, 6, 8, 8 ]
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5];
+var arr3 = arr1.concat(arr2);
+console.log(arr3);
+var half_length = Math.floor(arr3.length / 2);
+//console.log(half);
+var leftside = arr3.splice(0, half_length);
+console.log(leftside);
+var rightside = arr3.splice(half_length);
+console.log(rightside);
